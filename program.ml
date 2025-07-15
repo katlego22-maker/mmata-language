3d325f983e1f372424a6ef0232de1a7a99d7f757
katlego22-maker <mankgakatlego2@gmail.com>
2025-07-16 01:31:14 +0200

    Update program.ml

--- program.ml
+++ program.ml
@@ -1,9 +1,12 @@
-mmata
-ke rata lebelo = 50.
-tsa nou "Speed ke: " + lebelo.
-fetola lebelo = 100.
-tsa nou "New speed ke: " + lebelo.
+#lang: setswana
 
 mmata
+ke rata lebelo = 50.
+tsa nou "Lebelo la ntlha ke: " + lebelo.
+fetola lebelo = 100.
+tsa nou "Lebelo le le ntšha ke: " + lebelo.
+
+mmata
+tsa nou "Tlhakanya maina le dintlha tsa gago."
 kwala leina.
-tsa nou "Dumela " + leina.
+tsa nou "Dumela, " + leina.
