b020a8b8a6567b23e12ee91efab7cd3d01d77880
katlego22-maker <mankgakatlego2@gmail.com>
2025-07-16 02:57:16 +0200

    clean program.ml and rename outputs to expected_outputs.txt

--- program.ml
+++ program.ml
@@ -9,8 +9,3 @@ tsa nou "Lebelo le le ntšha ke: " + lebelo.
 mmata
 tsa nou "Tlhakanya maina le dintlha tsa gago."
 kwala leina.
-<<<<<<< HEAD
-tsa nou "Dumela, " + leina.
-=======
-tsa nou "Dumela, " + leina.
->>>>>>> 3d325f983e1f372424a6ef0232de1a7a99d7f757
